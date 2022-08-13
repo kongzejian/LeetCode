8b27c85089e4fd1e573b5eb88f81c535a99cd9fd
zejian <110577803+kongzejian@users.noreply.github.com>
2022-08-13 20:42:34 +0800

    Create Code.java

---
+++ 0001/Code.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+         if(map.containsKey(target-nums[i])){
+             int[] result=new int[]{i,map.get(target-nums[i])};
+             return result;
+         }
+         else{
+             map.put(nums[i],i);
+         }
+        }
+        return null;
+    }
+}
