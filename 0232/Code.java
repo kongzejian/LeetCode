6e64462fb0598d3830429eb47166b74af40eb4d6
zejian <110577803+kongzejian@users.noreply.github.com>
2022-08-20 17:50:02 +0800

    Create Code.java

---
+++ 0232/Code.java
@@ -0,0 +1,49 @@
+class MyQueue {
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+    public MyQueue() {
+        in=new Stack<>();
+        out=new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        if(!out.isEmpty()){
+            return out.pop();
+        }
+        else{
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+            return out.pop();
+        }
+    }
+
+    public int peek() {
+        if(!out.isEmpty()){
+            return out.peek();
+        }
+        else{
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+            return out.peek();
+        }
+    }
+
+    public boolean empty() {
+        return in.isEmpty()&&out.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
