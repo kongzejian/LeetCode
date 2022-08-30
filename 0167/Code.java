f392eb40aa1775a494905b338177a7abe11018eb
zejian <110577803+kongzejian@users.noreply.github.com>
2022-08-30 21:07:29 +0800

    Update Code.java

--- 0167/Code.java
+++ 0167/Code.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-          HashMap<Integer, Integer> h = new HashMap<>();
-        int[] result=new int[2];
+         HashMap<Integer, Integer> h = new HashMap<>();
         for(int i=0;i<numbers.length;i++){
             if(!h.containsKey(target-numbers[i])){
                 h.put(numbers[i],i );
             }
             else {
-                int index2=i+1;
-                int index1=h.get(target-numbers[i])+1;
-                result = new int[]{index1,index2};
-                break;
+                return new int[]{i+1,h.get(target-numbers[i])+1};
             }
         }
-        return result;
+        return new int[]{-1,-1};
     }
 }
