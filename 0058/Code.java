9d6972ebd9d9d5b93d83127840013f39c8a92d2d
zejian <110577803+kongzejian@users.noreply.github.com>
2022-09-06 20:51:09 +0800

    Update Code.java

--- 0058/Code.java
+++ 0058/Code.java
@@ -2,10 +2,10 @@ class Solution {
     public int lengthOfLastWord(String s) {
          int count = 0;
         int start = s.length() - 1;
-        while(start>=0 && Character.isSpaceChar(s.charAt(start))){  //先找到倒数第一个的字母                        //主要是Character.isSpaceChar(字符)的用法
+        while(start>=0 && Character.isSpaceChar(s.charAt(start))){  //先找到倒数第一个的字母                        //主要是Character.isSpaceChar(字符)的用法，也可以换成==' '  注意单引号且中间要敲一个空格
             start-=1;
         }
-        while(start>=0 && !Character.isSpaceChar(s.charAt(start))){ //找到倒数第一个字母前的第一个空格
+        while(start>=0 && !Character.isSpaceChar(s.charAt(start))){ //找到倒数第一个字母前的第一个空格 
             start-=1;
             count+=1;
         }
