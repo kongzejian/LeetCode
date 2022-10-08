75e0a9595dc8d3e5936405493e4336372aee8c5b
zejian <110577803+kongzejian@users.noreply.github.com>
2022-10-08 12:02:07 +0800

    Update Code.java

--- 0111/Code.java
+++ 0111/Code.java
@@ -15,19 +15,20 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        return min_depth(root);
+    }
+    public int min_depth(TreeNode root){
         if(root==null){
             return 0;
         }
-        int left_min_depth=minDepth(root.left);
-        int right_min_depth=minDepth(root.right);
-        if(root.left==null && root.right!=null){ //这里要特别注意，如果只有左子树，没有右子树，不能返回右子树为0，必须返回左子树+1
-            return right_min_depth+1;
+        int leftdepth=min_depth(root.left);
+        int rightdepth=min_depth(root.right);
+        if(leftdepth==0 && rightdepth==0){ //如果为叶结点返回1
+            return 1;
         }
-        else if(root.left!=null && root.right==null){ //这里要特别注意，如果只有右子树，没有左子树，不能返回左子树为0，必须返回右子树+1
-            return left_min_depth+1;
+        if(leftdepth==0 || rightdepth==0){ //如果只有左子树或只有右子树，不能返回最小值0，而是返回有子树的那个深度
+            return Math.max(leftdepth,rightdepth)+1;
         }
-        else{
-            return Math.min(left_min_depth,right_min_depth)+1;
-        }
+        return Math.min(leftdepth,rightdepth)+1; //有左子树也有右子树，返回最小深度+1
     }
 }
